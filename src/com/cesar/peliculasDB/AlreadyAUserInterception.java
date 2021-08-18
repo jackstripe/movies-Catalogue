3d13b086f9160b4f03424ddedea5fa4b82d0dd8d
Cesar Lovera <ingcesarfl@gmail.com>
2021-08-18 17:47:02 -0500

    Test commit

--- src/com/cesar/peliculasDB/AlreadyAUserInterception.java
+++ src/com/cesar/peliculasDB/AlreadyAUserInterception.java
@@ -1,4 +1,4 @@
-package com.cesar.peliculasDB;
+ package com.cesar.peliculasDB;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
@@ -13,7 +13,7 @@ public class AlreadyAUserInterception extends AbstractInterceptor {
 	//Validar que ese usuario no esté creado
 	public String intercept(ActionInvocation arg0) throws Exception {
 		// TODO Auto-generated method stub
-		
+		//this is really a test
 		return null;
 	}
 
